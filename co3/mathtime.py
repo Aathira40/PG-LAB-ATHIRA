40d6e32eeac039c56dc239d565442b4ba3449056
Aathira40 <93872710+Aathira40@users.noreply.github.com>
2021-12-13 11:53:44 +0530

    Delete mathtime.py

--- co3/mathtime.py
+++
@@ -1,6 +0,0 @@
-import time
-print("current time in sec",time.time())
-print("current time ",time.ctime())
-print("current time sfter 30 sec",time.ctime(time.time()+30))
-t=time.localtime()
-print(" time ",t)
