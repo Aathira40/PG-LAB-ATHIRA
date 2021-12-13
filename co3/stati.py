d5ec1460ab8723805d83fd092ece90b7a1ae492e
Aathira40 <93872710+Aathira40@users.noreply.github.com>
2021-12-13 12:37:51 +0530

    Add files via upload

---
+++ co3/stati.py
@@ -0,0 +1,12 @@
+import statistics
+l1=[4,7,8,2,3,4,5,7,8,9,0,7,1]
+a=statistics.mean(l1)
+print(a)
+b=statistics.median(l1)
+print(b)
+c=statistics.mode(l1)
+print(c)
+d=statistics.stdev(l1)
+print(d)
+e=statistics.variance(l1)
+print(e)
